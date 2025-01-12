cc09ceb2bc56fa52c4bb18b630d96dd4a54b0b5f
dusanskiljevic <d.skiljevic@futureforward.rs>
2025-01-12 13:51:15 +0100

    feat: Implement settings management for ActionKanban plugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import { Plugin } from 'obsidian';
 
-export default class ActionKanban extends Plugin {
+interface ActionKanbanSettings {
+  mySetting: string;
+}
 
+const DEFAULT_SETTINGS: ActionKanbanSettings = {
+  mySetting: 'default'
+}
+
+export default class ActionKanban extends Plugin {
+  settings: ActionKanbanSettings = DEFAULT_SETTINGS;
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }

cb51e9808fc056a0fdd5d9a5305760de5e0f10cb
dusanskiljevic <d.skiljevic@futureforward.rs>
2025-01-12 21:17:57 +0100

    feat: add settings tab for ActionKanban plugin configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface ActionKanbanSettings {
   mySetting: string;
@@ -11,11 +11,44 @@ const DEFAULT_SETTINGS: ActionKanbanSettings = {
 export default class ActionKanban extends Plugin {
   settings: ActionKanbanSettings = DEFAULT_SETTINGS;
 
+  async onload(): Promise<void> {
+    await this.loadSettings();
+
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new ActionKanbanSettingsTab(this.app, this));
+  }
+
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
   }
 
   async saveSettings() {
     await this.saveData(this.settings);
+  }
+}
+
+class ActionKanbanSettingsTab extends PluginSettingTab {
+  plugin: ActionKanban;
+
+  constructor(app: App, plugin: ActionKanban) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName('Setting #1')
+      .setDesc('It\'s a secret')
+      .addText(text => text
+        .setPlaceholder('Enter your secret')
+        .setValue(this.plugin.settings.mySetting)
+        .onChange(async (value) => {
+          this.plugin.settings.mySetting = value;
+          await this.plugin.saveSettings();
+        }));
   }
 }
